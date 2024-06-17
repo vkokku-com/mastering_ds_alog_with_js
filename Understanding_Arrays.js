ae2cca034766eb2418298e439dde0a1ef690dde1
Venkata Saikiran Kokku <venkatasaikiran.kokku@outlook.com>
2024-06-17 23:44:32 +0530

    Two dimensional arrays

--- Understanding_Arrays.js
+++ Understanding_Arrays.js
@@ -363,3 +363,32 @@ function afterc(str) {
 var words = ["recieve", "deceive", "percieve", "deceit", "concieve"];
 var misspelled = words.filter(afterc);
 console.log(words, misspelled); // displays recieve,percieve,concieve
+
+//Two Dimensional & Multi Dimensional Arrays
+//Creating Two Dimensional Arrays
+var twod = [];
+var rows = 5;
+for (var i = 0; i < rows; ++i) {
+    twod[i] = [];
+}
+
+Array.matrix = function (numrows, numcols, initial) {
+    var arr = [];
+    for (var i = 0; i < numrows; ++i) {
+        var columns = [];
+        for (var j = 0; j < numcols; ++j) {
+            columns[j] = initial;
+        }
+        arr[i] = columns;
+    }
+    return arr;
+}
+
+var nums = Array.matrix(5,5,0);
+console.log(nums[1][1]); // displays 0
+var names = Array.matrix(3,3,"");
+names[1][2] = "Joe";
+console.log(names[1][2]); // display "Joe"
+
+var grades = [[89, 77, 78],[76, 82, 81],[91, 94, 89]];
+console.log(grades,grades[2][2]); // displays 89

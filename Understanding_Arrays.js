0cab6d1c359d9959439628e974db171c7669fe56
Venkata Saikiran Kokku <venkatasaikiran.kokku@outlook.com>
2024-06-17 22:38:35 +0530

    Iterators return array

--- Understanding_Arrays.js
+++ Understanding_Arrays.js
@@ -306,4 +306,60 @@ var words = ["the ", "quick ", "brown ", "fox "];
 var sentence = words.reduceRight(concat);
 console.log(words, sentence);
 
+//Iterators functions that return a new array
+//map - method
+function curve(grade) {
+    return grade += 5;
+}
+var grades = [77, 65, 81, 92, 83];
+var newgrades = grades.map(curve);
+console.log(grades, newgrades); // 82, 70, 86, 97, 88
 
+function first(word) {
+    return word[0];
+}
+var words = ["for", "your", "information"];
+var acronym = words.map(first);
+console.log(acronym, acronym.join("")); // displays "fyi"
+
+//filter - method
+function isEven(num) {
+    return num % 2 == 0;
+}
+function isOdd(num) {
+    return num % 2 != 0;
+}
+var nums = [];
+for (var i = 0; i < 20; ++i) {
+    nums[i] = i + 1;
+}
+var evens = nums.filter(isEven);
+console.log("Even numbers: ");
+console.log(evens);
+var odds = nums.filter(isOdd);
+console.log("Odd numbers: ");
+console.log(odds);
+
+function passing(num) {
+    return num >= 60;
+}
+var grades = [];
+for (var i = 0; i < 20; ++i) {
+    grades[i] = Math.floor(Math.random() * 101);
+
+}
+var passGrades = grades.filter(passing);
+console.log("All grades:");
+console.log(grades);
+console.log("Passing grades: ");
+console.log(passGrades);
+
+function afterc(str) {
+    if (str.indexOf("cie") > -1) {
+        return true;
+    }
+    return false;
+}
+var words = ["recieve", "deceive", "percieve", "deceit", "concieve"];
+var misspelled = words.filter(afterc);
+console.log(words, misspelled); // displays recieve,percieve,concieve

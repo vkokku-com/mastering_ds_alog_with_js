2d85947827bd930fb276e5d574692bd7dd56782a
Venkata Saikiran Kokku <venkatasaikiran.kokku@outlook.com>
2024-06-18 23:03:49 +0530

    Processing Two Dimensional Arrays

--- Understanding_Arrays.js
+++ Understanding_Arrays.js
@@ -384,11 +384,40 @@ Array.matrix = function (numrows, numcols, initial) {
     return arr;
 }
 
-var nums = Array.matrix(5,5,0);
+var nums = Array.matrix(5, 5, 0);
 console.log(nums[1][1]); // displays 0
-var names = Array.matrix(3,3,"");
+var names = Array.matrix(3, 3, "");
 names[1][2] = "Joe";
 console.log(names[1][2]); // display "Joe"
 
-var grades = [[89, 77, 78],[76, 82, 81],[91, 94, 89]];
-console.log(grades,grades[2][2]); // displays 89
+var grades = [[89, 77, 78], [76, 82, 81], [91, 94, 89]];
+console.log(grades, grades[2][2]); // displays 89
+
+//Processing Two Dimensional Arrays
+var grades = [[89, 77, 78], [76, 82, 81], [91, 94, 89]];
+var total = 0;
+var average = 0.0;
+for (var row = 0; row < grades.length; ++row) {
+    for (var col = 0; col < grades[row].length; ++col) {
+        total += grades[row][col];
+    }
+    average = total / grades[row].length;
+    console.log("Student " + parseInt(row + 1) + " average: " +
+        average.toFixed(2));
+    total = 0;
+    average = 0.0;
+}
+
+var grades = [[89, 77, 78], [76, 82, 81], [91, 94, 89]];
+var total = 0;
+var average = 0.0;
+for (var col = 0; col < grades.length; ++col) {
+    for (var row = 0; row < grades[col].length; ++row) {
+        total += grades[row][col];
+    }
+    average = total / grades[col].length;
+    console.log("Test " + parseInt(col + 1) + " average: " +
+        average.toFixed(2));
+    total = 0;
+    average = 0.0;
+}
